c681831517bb5bed3bd974a5dc138dc8aa9750c2
Erivalenti <erica.valenti@waltonmarketltd.email>
2025-10-09 18:38:00 +0300

    Update env.js

--- js/env.js
+++ js/env.js
@@ -1,5 +1,6 @@
 // js/env.js
 window.__env = {
   SUPABASE_URL: "https://geazefwmcocuotixqlyf.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdlYXplZndtY29jdW90aXhxbHlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk5MTg3MTYsImV4cCI6MjA3NTQ5NDcxNn0.r0jfc5EITj47Hve-IuSYABfTDz4_-_5j76AYrpptf68"
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdlYXplZndtY29jdW90aXhxbHlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk5MTg3MTYsImV4cCI6MjA3NTQ5NDcxNn0.r0jfc5EITj47Hve-IuSYABfTDz4_-_5j76AYrpptf68
+    "
 };
